40eca6361afa5221b03ed1944689dd5c5820b743
AndargWorku12 <your.email@example.com>
2024-01-17 05:31:58 -0800

    add shop card

--- .history/src/pages/ShopCard_20240117053008.tsx
+++ .history/src/pages/ShopCard_20240117053008.tsx
@@ -77,13 +77,13 @@ const ShopCard:React.FC = () => {
     />
     <Stack mt='6' spacing='3'>
       <Heading size='md'>Living room Sofa</Heading>
-      <Text fontSize="sm">
+      <Text>
         This sofa is perfect for modern tropical spaces, baroque inspired
         spaces, earthy toned spaces and for people who love a chic design with a
         sprinkle of vintage design.
       </Text>
-      <Text color='blue.600' fontSize='sm'>
-        450 birr
+      <Text color='blue.600' fontSize='2xl'>
+        $450
       </Text>
     </Stack>
   </CardBody>
